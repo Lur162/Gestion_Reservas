acf3de4276b60fd693d9303e55672a62e5877b74
Lur162 <95856785+Lur162@users.noreply.github.com>
2023-05-17 15:44:29 +0200

    C Gmaill

---
+++ FCT/Reservas_Prueba1/Reservas_Prueba1/src/test/java/com/reservas/Reservas_Prueba1/ReservasPrueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.Reservas_Prueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasPrueba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
